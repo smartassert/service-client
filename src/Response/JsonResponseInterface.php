1824bc5cda966df420dabafb046cad453b1496ef
Jon Cram <webignition@gmail.com>
2022-10-26 10:54:48 +0100

    Remove JsonResponse inspection methods

--- src/Response/JsonResponseInterface.php
+++ src/Response/JsonResponseInterface.php
@@ -10,35 +10,4 @@ interface JsonResponseInterface extends ResponseInterface
      * @return array<mixed>
      */
     public function getData(): array;
-
-    /**
-     * @param non-empty-string $key
-     */
-    public function getString(string $key): ?string;
-
-    /**
-     * @param non-empty-string $key
-     */
-    public function getInteger(string $key): ?int;
-
-    /**
-     * @param non-empty-string $key
-     *
-     * @return null|non-empty-string
-     */
-    public function getNonEmptyString(string $key): ?string;
-
-    /**
-     * @param non-empty-string $key
-     *
-     * @return non-empty-string[]
-     */
-    public function getNonEmptyStringCollection(string $key): array;
-
-    /**
-     * @param non-empty-string $key
-     *
-     * @return null|positive-int
-     */
-    public function getPositiveInteger(string $key): ?int;
 }
