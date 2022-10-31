94aac7a6825be98be12a8ebcc2ae4d4ad88c4451
Jon Cram <webignition@gmail.com>
2022-10-31 10:33:27 +0000

    Add InvalidModelDataException

---
+++ src/Exception/InvalidModelDataException.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SmartAssert\ServiceClient\Exception;
+
+use Psr\Http\Message\ResponseInterface;
+use SmartAssert\ServiceClient\Response\JsonResponse;
+
+class InvalidModelDataException extends \Exception
+{
+    /**
+     * @param class-string $class
+     * @param array<mixed> $payload
+     */
+    public function __construct(
+        public readonly string $class,
+        public readonly ResponseInterface $response,
+        public readonly array $payload,
+    ) {
+        parent::__construct(sprintf('Data in response invalid for creating an instance of "%s"', $class));
+    }
+
+    /**
+     * @param class-string $class
+     *
+     * @throws InvalidResponseContentException
+     * @throws InvalidResponseDataException
+     */
+    public static function fromJsonResponse(string $class, JsonResponse $response): InvalidModelDataException
+    {
+        return new InvalidModelDataException($class, $response->getHttpResponse(), $response->getData());
+    }
+}
